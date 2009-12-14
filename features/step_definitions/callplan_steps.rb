79aed9e5a9f86c95ca25c6e2cceb1665d69a0c91
Joe Darling <joe.darling@bt.com>
2009-12-14 11:44:46 +0000

    formating

--- features/step_definitions/callplan_steps.rb
+++ features/step_definitions/callplan_steps.rb
@@ -4,12 +4,12 @@ Given /^we create a Callplan \(and store it's ID as <([^\)]*)>\) with these vari
     args[hash['name']] = hash['value']
   end
   callplan = Factory :callplan, :company_name => "Di Bread the baker"
-  Factory :inbound_number_manager, 
-    :phone_number => args['inbound_phone_number'], 
+  Factory :inbound_number_manager,
+    :phone_number => args['inbound_phone_number'],
     :callplan_id => callplan.id
-  action = Factory :action, 
+  action = Factory :action,
     :application_name => args['Action_type'],
-    :application_data => args['Action_params'], 
+    :application_data => args['Action_params'],
     :callplan_id => callplan.id
   feature_vars['callplan_id'] = callplan.id
   feature_vars['action_id'] = action.id
@@ -20,17 +20,17 @@ Given /^we have an IVR Menu with:$/ do |table|
   table.hashes.each do |hash|
     args[hash['name']] = hash['value']
   end
-  ivr_menu = Factory :ivr_menu, 
+  ivr_menu = Factory :ivr_menu,
     :long_greeting => args['long_greeting'],
     :action_id => feature_vars['action_id']
-  Factory :ivr_menu_entry, 
+  Factory :ivr_menu_entry,
     :digits => args['ivr_menu_entry1_digit'],
-    :user_param_part => args['ivr_menu_entry1_action'], 
+    :user_param_part => args['ivr_menu_entry1_action'],
     :prompt => args['ivr_menu_entry1_prompt'],
     :ivr_menu_id => ivr_menu.id
-  Factory :ivr_menu_entry, 
+  Factory :ivr_menu_entry,
     :digits => args['ivr_menu_entry2_digit'],
-    :user_param_part => args['ivr_menu_entry2_action'], 
+    :user_param_part => args['ivr_menu_entry2_action'],
     :prompt => args['ivr_menu_entry2_prompt'],
     :ivr_menu_id => ivr_menu.id
 end
