6416a3276266bc46af616b87a22fee058ab91a9e
Joe Darling <joe.darling@bt.com>
2010-06-15 22:33:34 +0100

    removed the homepage when logged in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,9 @@ class HomeController < ApplicationController
 
   def index
     @tab = 'home'
+    if signed_in? && session['callplan_id']
+      @tab = 'callplan'
+      redirect_to callplan_url(session['callplan_id'])
+    end
   end
-
 end
--- spec/controllers/home_controller_spec.rb
+++ spec/controllers/home_controller_spec.rb
@@ -1,10 +1,30 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe HomeController do
-
-  #Delete this example and add some real ones
-  it "should use HomeController" do
-    controller.should be_an_instance_of(HomeController)
+  def do_get 
+    get :index
   end
 
+  it "should set the tab" do
+    do_get 
+    assigns[:tab].should == "home"
+  end
+
+  context "if the user is logged in" do
+    before do
+      controller.stub(:signed_in?).and_return true
+      @callplan_id = 1
+      session[:callplan_id] = @callplan_id
+    end
+
+    it "should set the tab" do
+      do_get 
+      assigns[:tab].should == "callplan"
+    end
+
+    it "should redirect to the callplan page" do
+      do_get 
+      controller.should redirect_to callplan_url(@callplan_id)
+    end
+  end
 end
