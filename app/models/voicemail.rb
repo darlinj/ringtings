0a24e872e9870a02139a69c96e78ae71156c0319
Joe Darling <joe.darling@bt.com>
2010-08-18 21:53:44 +0100

    Voicemail: now the voicemail retrieval works for a single voicemail

--- app/models/voicemail.rb
+++ app/models/voicemail.rb
@@ -10,6 +10,26 @@ class Voicemail
   def get
     options = { :basic_auth => @auth }
     response = self.class.get("#{VOICEMAIL_URI}", options)
+    [parse_voicemail_details_from_response(Nokogiri::HTML(response).xpath("//tr[2]"))]
+  end
+
+  private
+
+  def parse_voicemail_details_from_response response
+    response_text = response.xpath("text()").to_s
+    from = response.xpath("//div[@class='title']/b/text()").to_s.split[3]
+    date_created = response_text[/Created:.*/].gsub("Created: ", "")
+    priority = response_text[/Priority:.*/].gsub("Priority: ", "")
+    last_heard = response_text[/Last Heard:.*/].gsub("Last Heard: ", "")
+    duration = response_text[/Duration:.*/].gsub("Duration: ", "")
+    filename = response.xpath("//a[contains(@href,'get')]/@href").to_s[/\w*\.\w*\Z/]
+    {:from => from,
+      :date_created => date_created,
+      :priority => priority,
+      :last_heard => last_heard,
+      :duration => duration,
+      :filename => filename
+    }
   end
 
 
--- spec/models/voicemail_spec.rb
+++ spec/models/voicemail_spec.rb
@@ -1,32 +1,63 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Voicemail do
-  #before do
-    #username = "Bob"
-    #password = "secret"
-    #@options = {:basic_auth => {:username => username, :password => password}}
+  before do
+    @username = "Bob"
+    @password = "secret"
+    @options = {:basic_auth => {:username => @username, :password => @password}}
 
-    #from = "0123456789"
-    #date_created = 1.hour.ago
-    #priority = "normal"
-    #last_heard = 10.minutes.ago
-    #duration = 10.seconds
-    #filename = "gibberish.wav"
+    vm1 = {:from => "0123456789",
+           :date_created => 1.hour.ago.to_s,
+           :priority => "normal",
+           :last_heard => 10.minutes.ago.to_s,
+           :duration => 10.seconds.to_s,
+           :filename => "gibberish.wav"
+          }
+
+    @voicemail_response = %Q{
+<title>FreeSWITCH Voicemail</title>
+<body bgcolor=eeeeee>
+
+<table bgcolor=ffffff width=75% align=center style="border-style:inset;border-width:2px">
+<tr><td bgcolor=1010ff align=center valign=center style="border-style:inset;border-width:2px">
+<font face=arial size=+2 color=ffffff>Voicemail Messages</font>
+</td></tr>
+<tr><td>
+
+<font face=tahoma><div class=title><b>Message from Extension #{vm1[:from]} 1000</b></div><hr noshade size=1>
+Priority: #{vm1[:priority]}<br>
+Created: #{vm1[:date_created]}<br>
+Last Heard: #{vm1[:last_heard]}<br>
+Duration: #{vm1[:duration]}<br>
+<br><object width=550 height=15
+type="application/x-shockwave-flash"
+data="http://192.168.0.4:8080/pub/slim.swf?song_url=http://192.168.0.4:8080/api/voicemail/get/#{vm1[:filename]}&player_title=Extension%201000%20%3C1000%3E%2007/24/10%2008%3A40%3A10">
+<param name=movie value="http://192.168.0.4:8080/pub/slim.swf?song_url=http://192.168.0.4:8080/api/voicemail/get/#{vm1[:filename]}&player_title=Extension%201000%20%3C1000%3E%2007/24/10%2008%3A40%3A10"></object><br><br>
+[<a href=http://192.168.0.4:8080/api/voicemail/del/#{vm1[:filename]}>delete</a>] [<a href=http://192.168.0.4:8080/api/voicemail/get/#{vm1[:filename]}>download</a>] [<a href=tel:1000>call</a>] <br><br><br></font>
+1 message<br>
+</td></tr>
+</table>
+}
+
+    Voicemail.stub(:get).with(VOICEMAIL_URI, @options).and_return(@voicemail_response)
+
+    @voicemails = [vm1]
+  end
+
+  def do_action
+    Voicemail.new(@username,@password).get
+  end
 
 
-    #Voicemail.stub(:get).with(VOICEMAIL_URI, @options)
-    #@voicemails = [vm1,vm2]
-  #end
-  #describe "#find_all" do
-    #it "should set the authentication credentials" do
-      #Voicemail.should_receive(:get).with(VOICEMAIL_URI, @options)
-      #Voicemail.new(username,password).get
-    #end
+  describe "#find_all" do
+    it "should set the authentication credentials" do
+      Voicemail.should_receive(:get).with(VOICEMAIL_URI, @options)
+      do_action
+    end
 
-    #it "should return the list of voicemails" do
+    it "should return the list of voicemails" do
+      do_action.should == @voicemails
+    end
 
-      #Voicemail.new(username,password).get.should == @voicemails
-    #end
-
-  #end
-end 
+  end
+end
