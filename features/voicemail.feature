95410f2a881b0b1f8d9194ac06d2b2a70a0d3460
Joe Darling <joe.darling@bt.com>
2010-08-31 09:55:46 +0100

    fixed voicemail display

--- features/voicemail.feature
+++ features/voicemail.feature
@@ -1,11 +1,11 @@
 Feature: Voicemail file management
   Scenario: Listing your voicemail
    Given I have signed in with "joe@foo.com/secret"
-    And we create a standard Callplan
-    And the callplan is owned by "joe@foo.com"
-    And I mock 3 voicemail messages in my voicemail
-    When I follow "voicemail"
-    Then the response should have 3 elements that match "//div[@class='voicemail']"
+     And we create a standard Callplan
+     And the callplan is owned by "joe@foo.com"
+     And I mock 3 voicemail messages in my voicemail
+   When I follow "voicemail"
+   Then the response should have 3 elements that match "//div[@class='voicemail']/table/tbody/tr"
 
   Scenario: showing an empty list
     Given I have signed in with "joe@foo.com/secret"
--- spec/views/freeswitch/directory.xml.builder_spec.rb
+++ spec/views/freeswitch/directory.xml.builder_spec.rb
@@ -5,6 +5,8 @@ describe "/service_instances/directory.xml.builder" do
   before do
     @number_matcher = "1234567890"
     assigns[:inbound_number] = @number_matcher
+    @inbound_number_manager = Factory.create(:inbound_number_manager)
+    assigns[:inbound_number_manager] =  @inbound_number_manager
   end
 
   def do_render
@@ -26,29 +28,6 @@ describe "/service_instances/directory.xml.builder" do
     xml = do_render
     xml.elements['/document/section/domain/@name'].value.should == '$${domain}'
   end
-    #<groups>
-      #<group name="default">
-        #<users>
-           #<user id="1000">
-            #<params>
-              #<param name="password" value="$${default_password}"/>
-              #<param name="vm-password" value="1000"/>
-            #</params>
-            #<variables>
-              #<variable name="toll_allow" value="domestic,international,local"/>
-              #<variable name="accountcode" value="1000"/>
-              #<variable name="user_context" value="default"/>
-              #<variable name="effective_caller_id_name" value="Extension 1000"/>
-              #<variable name="effective_caller_id_number" value="1000"/>
-              #<variable name="outbound_caller_id_name" value="$${outbound_caller_name}"/>
-              #<variable name="outbound_caller_id_number" value="$${outbound_caller_id}"/>
-              #<variable name="callgroup" value="techsupport"/>
-            #</variables>
-          #</user>
-
-        #</users>
-      #</group>
-
 
   it 'should render a group element with an name of default' do
     xml = do_render
@@ -58,6 +37,16 @@ describe "/service_instances/directory.xml.builder" do
   it 'should render a user element with an id of the @number_matcher' do
     xml = do_render
     xml.elements['/document/section/domain/groups/group/users/user/@id'].value.should == @number_matcher
+  end
+
+  it 'should render a http-allowed-api element element with the value of voicemail' do
+    xml = do_render
+    xml.elements['/document/section/domain/groups/group/users/user/params/param/@http-allowed-api'].value.should == "voicemail"
+  end
+
+  it 'should render a voicemail password element element with the correct value' do
+    xml = do_render
+    xml.elements['/document/section/domain/groups/group/users/user/params/param/@vm-password'].value.should == @inbound_number_manager.voicemail_password
   end
 end
 
