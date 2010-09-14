42cfad8367d895de8921a0272893fe33d66d66b4
Joe Darling <joe.darling@bt.com>
2010-09-14 20:43:34 +0100

    Voicemail Delete: add routing

--- spec/routing_spec.rb
+++ spec/routing_spec.rb
@@ -10,6 +10,7 @@ describe 'Routing' do
   it { should route(:get,   "/demo_callplans/1"                            ).to(:controller => 'demo_callplans', :action => 'show', :id=>1) }
   it { should route(:get,   "/voicemail"                                   ).to(:controller => 'voicemail', :action => 'index') }
   it { should route(:get,   "/voicemail/foo"                               ).to(:controller => 'voicemail', :action => 'show', :id => 'foo') }
+  it { should route(:delete,"/voicemail/foo"                               ).to(:controller => 'voicemail', :action => 'destroy', :id => 'foo') }
   it { should route(:get,   "/callplans/1"                                 ).to(:controller => 'callplans', :action => 'show', :id=> '1') }
   it { should route(:put,   "/callplans/1"                                 ).to(:controller => 'callplans', :action => 'update', :id=> '1') }
   it { should route(:delete,"/ivr_menu_entries/1"                          ).to(:controller => 'ivr_menu_entries', :action => 'destroy', :id=>1) }
