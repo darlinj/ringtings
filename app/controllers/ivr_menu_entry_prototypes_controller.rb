26033a45c6a51a2a7bd53f4067274c52fa957c7b
joe <joe.darling@bt.com>
2011-02-24 20:33:11 +0000

    fixes for IE

--- app/controllers/ivr_menu_entry_prototypes_controller.rb
+++ app/controllers/ivr_menu_entry_prototypes_controller.rb
@@ -3,7 +3,7 @@ class IvrMenuEntryPrototypesController < ApplicationController
     @ivr_menu_entry_prototypes = IvrMenuEntryPrototype.all
     @ivr_menu_id = params['ivr_menu_id']
     respond_to do |format|
-      format.html { render :template =>"ivr_menu_entry_prototypes/index" }
+      format.html { render :template =>"ivr_menu_entry_prototypes/index", :layout => false }
       format.js { render :template =>"ivr_menu_entry_prototypes/index.js", :layout => false }
     end
   end
